cf6ec618c44df73e2dde220fdda6e50a6370a2ef
Kris Sankaran <sankaran.kris@gmail.com>
2022-06-01 14:19:02 -0500

    update processing script

--- cluster/process_sample.sh
+++ cluster/process_sample.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 tar -zxvf humann_databases.tar.gz
+tar -zxvf databases/full_chocophlan.v296_201901b.tar.gz
+tar -zxvf databases/full_mapping_v201901b.tar.gz
+tar -zxvf databases/uniref90_annotated_v201901b_full.tar.gz
 
 export f=$(sed -n "$id p" SRR_Acc_List.txt)
 prefetch $f
@@ -17,5 +20,5 @@ tar -zcvf ${f}_processed ${f}.tar.gz
 
 # these are the commands for downloading the files that were passed as input above
 #humann_databases --download chocophlan full $db_path --update-config yes
-#humann_databases --download uniref $db_path --update-config yes
+#humann_databases --download uniref uniref90_diamond $db_path --update-config yes
 #humann_databases --download utility_mapping full $db_path --update-config yes
