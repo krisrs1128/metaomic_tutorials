7c43dd83c2d25190a2e63b859c0b907e5e71d854
Kris Sankaran <sankaran.kris@gmail.com>
2022-06-02 15:33:54 -0500

    updater sample

--- cluster/process_sample.sh
+++ cluster/process_sample.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-tar -zxvf humann_databases.tar.gz
-tar -zxvf databases/full_chocophlan.v296_201901b.tar.gz
-tar -zxvf databases/full_mapping_v201901b.tar.gz
-tar -zxvf databases/uniref90_annotated_v201901b_full.tar.gz
+export db_path=/staging/ksankaran/databases
+
+cp $db_path/full_chocophlan.v296_201901b.tar.gz .
+cp $db_path/full_mapping_v201901b.tar.gz .
+cp $db_path/uniref90_annotated_v201901b_full.tar.gz .
+
+tar -zxvf full_chocophlan.v296_201901b.tar.gz
+tar -zxvf full_mapping_v201901b.tar.gz
+tar -zxvf uniref90_annotated_v201901b_full.tar.gz
 
 export f=$(sed -n "$id p" SRR_Acc_List.txt)
 prefetch $f
 fastq-dump $f
 
 # use the previously downloaded databases
-export db_path=/staging/ksankaran/databases
-humann_config --update database_folders nucleotide $db_path/chocophlan
-humann_config --update database_folders protein $db_path/uniref
-humann_config --update database_folders pathways $db_path/metacyc
+humann_config --update database_folders nucleotide chocophlan
+humann_config --update database_folders protein uniref
+humann_config --update database_folders pathways metacyc
 
 humann -i ${f}.fastq -o ${f}_processed
 tar -zcvf ${f}_processed ${f}.tar.gz
